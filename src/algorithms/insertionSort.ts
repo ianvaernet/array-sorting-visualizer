314ba98eb00f9ae34c5d64e9f052fba9237a9371
Ian Vaernet <ianvaernet@gmail.com>
2022-04-28 11:50:12 -0300

    insertionSort animation working

--- src/algorithms/insertionSort.ts
+++ src/algorithms/insertionSort.ts
@@ -5,25 +5,25 @@ const WIDTH = 65;
 
 export const insertionSort = async (inputArray: Block[], setArray: React.Dispatch<React.SetStateAction<Block[]>>) => {
   const renderedArray = inputArray.slice();
-  const array = inputArray.map(({ number }) => number);
-  for (let indexToSort = 1; indexToSort < array.length; indexToSort++) {
-    let elementToSort = array[indexToSort];
-    renderedArray[indexToSort].y += WIDTH;
+  const arrayToSort = inputArray.map(({ number }, index) => ({ number, originalPosition: index }));
+  for (let indexToSort = 1; indexToSort < arrayToSort.length; indexToSort++) {
+    let elementToSort = arrayToSort[indexToSort];
+    renderedArray[arrayToSort[indexToSort].originalPosition].y += WIDTH;
     setArray(renderedArray.slice());
     await sleep(200);
     let j = indexToSort - 1;
-    while (j > -1 && elementToSort < array[j]) {
-      array[j + 1] = array[j];
-      renderedArray[j].x += WIDTH;
+    while (j > -1 && elementToSort.number < arrayToSort[j].number) {
+      arrayToSort[j + 1] = arrayToSort[j];
+      renderedArray[arrayToSort[j].originalPosition].x += WIDTH;
       setArray(renderedArray.slice());
       await sleep(200);
-      renderedArray[indexToSort].x -= WIDTH;
+      renderedArray[elementToSort.originalPosition].x -= WIDTH;
       setArray(renderedArray.slice());
       await sleep(200);
       j--;
     }
-    array[j + 1] = elementToSort;
-    renderedArray[indexToSort].y -= WIDTH;
+    arrayToSort[j + 1] = elementToSort;
+    renderedArray[elementToSort.originalPosition].y -= WIDTH;
     setArray(renderedArray.slice());
     await sleep(200);
   }
