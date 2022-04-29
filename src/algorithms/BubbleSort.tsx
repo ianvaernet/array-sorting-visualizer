ffc3c6e69b5033cc0ca4c02c2a6484f4a8c4e3d9
Bryan Henry López Maldonado <bryanlm71@gmail.com>
2022-04-29 10:10:50 -0400

    add the first version of bubble sort

--- src/algorithms/BubbleSort.tsx
+++ src/algorithms/BubbleSort.tsx
@@ -4,19 +4,24 @@ export const BubbleSort = async (blocksArray: Block[], { moveUp, moveDown, moveL
 
   const array = blocksArray.map(({ number }, index) => ({ number, originalPosition: index }));
   const length = array.length;
-  for (let indexToSort = 0; indexToSort < length ; indexToSort++) {
+  for (let indexToSort = 0; indexToSort < length - 1 ; indexToSort++) {
     let elementToSort = array[indexToSort];
     await moveDown(elementToSort.originalPosition);
-    let indexToCompare = indexToSort - 1;
-    
-    while (indexToCompare > -1 && elementToSort.number < array[indexToCompare].number) {
-      array[indexToCompare + 1] = array[indexToCompare];
-      await moveRight(array[indexToCompare].originalPosition);
-      await moveLeft(elementToSort.originalPosition);
-      indexToCompare--;
+    for (let indexToCompare = 0; indexToCompare < length - 1; indexToCompare++) {
+      if (array[indexToCompare].number >  array[indexToCompare + 1].number) {
+        
+        var swap = array[indexToCompare];
+        array[indexToCompare] = array[indexToCompare + 1 ]
+        array[indexToCompare + 1 ] = swap;
+        await moveRight(array[indexToCompare+1].originalPosition);
+        await moveLeft(swap.originalPosition);
+        indexToCompare--
+      }
     }
-    array[indexToCompare + 1] = elementToSort;
+    
+
     await moveUp(elementToSort.originalPosition);
+    
   }
   return blocksArray;
 
